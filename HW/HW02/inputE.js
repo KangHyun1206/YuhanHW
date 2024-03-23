6311bdb54033be1956ef833235d479edc17bd1ff
KangHyun1206 <kangh1206@naver.com>
2024-03-23 13:20:48 +0900

    renewFile

---
+++ HW/HW02/inputE.js
@@ -0,0 +1,40 @@
+// var canvas = document.getElementById("myCanvas");
+// var context = canvas.getContext("2d");
+
+
+
+// context.beginPath();
+// context.strokeStyle="black";
+// context.arc(250,250,200,0,Math.PI*2);
+// context.stroke();
+
+function circle1()
+{
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    var emptyview = document.getElementById("firstview");
+    var imgview = document.getElementById("gameImg");
+    var gameview = document.getElementById("instructionSection");
+
+    emptyview.style.display = "none";
+    imgview.style.display = "none";
+    gameview.style.display = "none";
+    myCanvas.style.display = "block";
+
+    context.beginPath();
+    context.strokeStyle="black";
+    context.arc(250,250,200,0,Math.PI*2);
+    context.stroke();
+}
+function review1()
+{
+    var imgview = document.getElementById("gameImg");
+    var gameview = document.getElementById("instructionSection");
+    var emptyview = document.getElementById("firstview");
+    var canvas = document.getElementById("myCanvas");
+
+    myCanvas.style.display = "none";
+    emptyview.style.display = "none";
+    gameview.style.display = "flex";
+    imgview.style.display = "block";
+}
